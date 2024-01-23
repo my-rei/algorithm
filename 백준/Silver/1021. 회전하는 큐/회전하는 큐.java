0227d7eabce150e1a7e2e538820d810be757add7
Yerim <77157624+rime-y@users.noreply.github.com>
2024-01-23 15:47:33 +0900

    [Silver III] Title: 회전하는 큐, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -19,44 +19,25 @@ public class Main {
 		int cnt = 0;
 		for (int k = 0; k < Integer.parseInt(nm[1]); k++) {
 			int target = Integer.parseInt(nums[k]);
-			int leftCnt = 0, rightCnt = 0;
 
 			while (dq.size() > 0) {
-//				System.out.println(((LinkedList<Integer>)dq).indexOf(target) + " : " + dq.size() / 2);
 				if (dq.peek() == target) {
 					dq.pop();
 					break;
 				} else if (((LinkedList<Integer>)dq).indexOf(target) > dq.size() / 2) {
 					dq.offerFirst(dq.pollLast());
 					cnt++;
-//					System.out.println(dq);
 					continue;
 				} else if (((LinkedList<Integer>)dq).indexOf(target) < dq.size() / 2) {
 					dq.offerLast(dq.pollFirst());
 					cnt++;
-//					System.out.println(dq);
 					continue;
 				} else if (((LinkedList<Integer>)dq).indexOf(target) == dq.size() / 2) {
 					dq.offerLast(dq.pollFirst());
 					cnt++;
-//					System.out.println(dq);
 					continue;
-//					if (k != Integer.parseInt(nm[1]) - 1 && ((LinkedList<Integer>)dq).indexOf(Integer.parseInt(nums[k+1])) > dq.size() / 2) {
-//						System.out.println("here");
-//						dq.offerFirst(dq.pollLast());
-//						cnt++;
-//						System.out.println(dq);
-//						continue;
-//					} else {
-//						dq.offerLast(dq.pollFirst());
-//						cnt++;
-//						System.out.println(dq);
-//						continue;
-//					}
 				}
 			}
-
-//			cnt += Math.min(leftCnt, rightCnt);
 		}
 		System.out.println(cnt);
 
