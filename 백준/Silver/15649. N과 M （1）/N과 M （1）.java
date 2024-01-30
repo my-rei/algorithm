bb41f1471b9bfd110b5608d9a5cd43cd1776538f
Yerim <77157624+rime-y@users.noreply.github.com>
2024-01-30 14:35:39 +0900

    [Silver III] Title: N과 M (1), Time: 328 ms, Memory: 30980 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 public class Main {
 	static int[] isSelected;
 	static int n, m;
-	static int[][] numPrint;
-	static int colCnt = 0;
 	static StringBuilder sb;
 
 	public static void main(String[] args) throws Exception {
@@ -17,22 +15,7 @@ public class Main {
 
 		isSelected = new int[n];
 
-		numPrint = new int[70][70];
-		colCnt = 0;
-
 		findSeq(0, "");
-
-//		for (int i = 0; i < numPrint.length; i++) {
-//			if (numPrint[i][0] == 0)
-//				break;
-//			for (int j = 0; j < numPrint[i].length; j++) {
-//				if (numPrint[i][j] == 0) {
-//					sb.append("\n");
-//					break;
-//				}
-//				sb.append(numPrint[i][j] + " ");
-//			}
-//		}
 
 		System.out.println(sb);
 	}
@@ -46,14 +29,7 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			if (isSelected[i] == 0) {
 				isSelected[i] = 1;
-//				for (int j = 0; j < 70; j++) {
-//					if (numPrint[colCnt][j] == 0) {
-//						numPrint[colCnt][j] = i + 1;
-//						break;
-//					}
-//				}
 				findSeq(cnt + 1, res + (i + 1) + " ");
-				colCnt++;
 				isSelected[i] = 0;
 
 			}
