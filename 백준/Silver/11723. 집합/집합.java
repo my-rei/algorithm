5738e6f98f399fbf70fb9d2d5e3e100e6d9ef9b2
Yerim <77157624+rime-y@users.noreply.github.com>
2024-01-31 17:40:11 +0900

    [Silver V] Title: 집합, Time: 1372 ms, Memory: 314008 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -6,38 +5,37 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
+	// 배열 사용
+	static boolean[] map = new boolean[20];
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		Set<String> s = new HashSet<>();
-		
+
 		int M = Integer.parseInt(br.readLine());
-		while(M-->0) {
+		while (M-- > 0) {
 			String[] order = br.readLine().split(" ");
-			
-			switch(order[0]) {
+
+			switch (order[0]) {
 			case "add":
-				s.add(order[1]);
+				map[Integer.parseInt(order[1]) - 1] = true;
 				break;
 			case "remove":
-				if (s.contains(order[1]))
-					s.remove(order[1]);
+				map[Integer.parseInt(order[1]) - 1] = false;
 				break;
 			case "check":
-				sb.append((s.contains(order[1])? 1:0)+"\n");
+				sb.append((map[Integer.parseInt(order[1]) - 1] ? 1 : 0) + "\n");
 				break;
 			case "toggle":
-				if (s.contains(order[1]))
-					s.remove(order[1]);
-				else
-					s.add(order[1]);
+				map[Integer.parseInt(order[1]) - 1] = !map[Integer.parseInt(order[1]) - 1];
 				break;
 			case "all":
-				for(int i = 1;i<=20;i++)
-					s.add(String.valueOf(i));
+				for (int i = 0; i < 20; i++)
+					map[i] = true;
 				break;
 			case "empty":
-				s.clear();
+				for (int i = 0; i < 20; i++)
+					map[i] = false;
 				break;
 			}
 		}
