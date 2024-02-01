015ae8aefca96cade15e5a6b8f2edab0e1594079
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-01 15:11:23 +0900

    [Gold V] Title: 신기한 소수, Time: 136 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,13 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 	static int N;
-	static List<Integer> odds;
-	static boolean[] oddsList;
 	static StringBuilder sb;
 
 	public static void main(String[] args) throws Exception {
@@ -15,17 +10,7 @@ public class Main {
 		sb = new StringBuilder();
 
 		N = Integer.parseInt(br.readLine());
-//		odds = new ArrayList<>();
-//		odds.add(2);
-//
-//		oddsList = new boolean[(int) Math.pow(10, N)];
-//		findAllOdd();
-//		for (int i = (int) Math.pow(10, N - 1); i < (int) Math.pow(10, N); i++) {
-//			if (!oddsList[i]) {
-//				System.out.println(i);
-//			}
-//
-//		}
+
 		for (int i = 1; i <= 9; i++) {
 			findOdd(1, i);
 		}
@@ -45,7 +30,6 @@ public class Main {
 
 		int[] next = new int[] { 1, 3, 7, 9 };
 		for (int n : next) {
-			// System.out.println(target*10+i);
 			findOdd(cnt + 1, target * 10 + n);
 		}
 	}
@@ -62,39 +46,5 @@ public class Main {
 		}
 
 		return true;
-	}
-
-	static void findAllOdd() {
-		// 에라토스테네스의 체
-		oddsList[0] = true;
-		oddsList[1] = true;
-
-		List<Integer> oddListTemp = new ArrayList<>();
-		for (int k = 1; k < N + 1; k++) {
-			for (int i = 2; i < Math.pow(10, k); i++) {
-				boolean flag = false;
-				for (int o : oddListTemp) {
-					if (i != o && i % o == 0) {
-						flag = true;
-					}
-				}
-				if (!flag) {
-					oddListTemp.add(i);
-				} else {
-					oddsList[i] = true;
-				}
-
-			}
-
-			for (int i = 1; i < Math.pow(10, k - 1); i++) {
-				if (oddsList[i]) {
-					for (int j = 0; j < 10; j++) {
-						oddsList[i * 10 + j] = true;
-					}
-				}
-			}
-
-		}
-
 	}
 }
