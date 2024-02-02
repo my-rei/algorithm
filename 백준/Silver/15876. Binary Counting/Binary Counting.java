fd90b6765efce4f2aec1daf1754fee3d6b3dd674
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-02 09:56:51 +0900

    [Silver V] Title: Binary Counting, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/15876. Binary Counting/Binary Counting.java
+++ 백준/Silver/15876. Binary Counting/Binary Counting.java
@@ -10,30 +10,31 @@ public class Main {
 		int n = Integer.parseInt(nk[0]);
 		int k = Integer.parseInt(nk[1]);
 
-		List<Integer> numList = new ArrayList<>();
+		int cnt = 0, startNum = 0, curNum = 0;
+		List<Integer> twoList = new ArrayList<>();
+		List<Integer> twoTemp = new ArrayList<>();
+		while (true) {
+			if (curNum == 0) {
+				curNum = startNum++;
+				for (int i = twoTemp.size() - 1; i > -1; i--) {
+					twoList.add(twoTemp.get(i));
+				}
+				twoTemp.clear();
+			}
 
-		int curNum = 0;
-		int curNumLim = 1;
-		int curCnt = 0;
-		int maxLim = 5 * n;
-
-		for (int i = 0; i < maxLim; i++) {
-			curNumLim = curNum == 0 ? 0 : (int) Math.floor(Math.log(curNum) / Math.log(2));
-
-			numList.add((curNum & (int) (Math.pow(2, curCnt))) >> (curCnt));
-			// System.out.println("curNum " + curNum + " curNumLim " + curNumLim + " curCnt
-			// " + curCnt + " res "
-			// + ((curNum & (int) (Math.pow(2, curCnt))) >> (curCnt)));
-			if (curCnt <= 0) {
-				curNum++;
-				curCnt = curNum == 0 ? 0 : (int) Math.floor(Math.log(curNum) / Math.log(2));
-			} else
-				curCnt--;
-		}
-		for (int i = 0;i<5;i++) {
+			twoTemp.add(1 & curNum);
+			curNum = curNum >> 1;
 			
-			System.out.print(numList.get(i*n+k-1)+" ");
+			if(cnt++ > 5*n) 
+				if(curNum == 0)
+					break;
 		}
-		// System.out.println(numList);
+		for (int i = twoTemp.size() - 1; i > -1; i--) {
+			twoList.add(twoTemp.get(i));
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print(twoList.get(i * n + k - 1) + " ");
+		}
 	}
 }
