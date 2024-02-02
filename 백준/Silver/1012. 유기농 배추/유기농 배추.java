9011774f977dd42397a4a3490f21d8aa7c99b1c7
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-02 15:40:48 +0900

    [Silver II] Title: 유기농 배추, Time: 192 ms, Memory: 17244 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,63 +1,62 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
-		int[][] dXy = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-		for (int t = 0; t < T; t++) {
+
+		while (T-- > 0) {
 			String[] mnk = br.readLine().split(" ");
 			int m = Integer.parseInt(mnk[0]);
 			int n = Integer.parseInt(mnk[1]);
 			int k = Integer.parseInt(mnk[2]);
 
 			int[][] farm = new int[m][n];
-
-			for (int j = 0; j < k; j++) {
+			boolean[][] visited = new boolean[m][n];
+			for (int i = 0; i < k; i++) {
 				String[] xy = br.readLine().split(" ");
 				farm[Integer.parseInt(xy[0])][Integer.parseInt(xy[1])] = 1;
 			}
 
-			int snail = 0;
-
-			Queue<Integer[]> queue = new LinkedList<>();
+			Queue<Integer[]> queue = new ArrayDeque<>();
+			int cnt = 0;
+			int[][] dxy = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
 			for (int i = 0; i < m; i++) {
 				for (int j = 0; j < n; j++) {
-					if (farm[i][j] == 1) {
-						queue.clear();
-
+					if (farm[i][j] == 1 && visited[i][j] == false) {
 						queue.add(new Integer[] { i, j });
+						visited[i][j] = true;
+						cnt++;
 
 						while (!queue.isEmpty()) {
-							int x = queue.peek()[0];
-							int y = queue.peek()[1];
+							int curI = queue.peek()[0];
+							int curJ = queue.peek()[1];
 
-							for (int[] d : dXy) {
-								if (x + d[0] > -1 && x + d[0] < m && y + d[1] > -1 && y + d[1] < n) {
-									if (farm[x + d[0]][y + d[1]] == 1) {
-										queue.add(new Integer[] { x + d[0], y + d[1] });
-										farm[x + d[0]][y + d[1]] = 0;
-									}
+							for (int[] d : dxy) {
+								int tI = curI + d[0];
+								int tJ = curJ + d[1];
+
+								if (tI > -1 && tI < m && tJ > -1 && tJ < n && visited[tI][tJ] == false
+										&& farm[tI][tJ] == 1) {
+									queue.add(new Integer[] { tI, tJ });
+									visited[tI][tJ] = true;
 								}
 							}
 
 							queue.poll();
 						}
-
-						snail++;
 					}
+
 				}
 			}
-
-			sb.append(snail + "\n");
-
+			sb.append(cnt + "\n");
 		}
-
 		System.out.println(sb);
 
 	}
