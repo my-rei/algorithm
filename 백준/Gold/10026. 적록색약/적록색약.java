d8233b195e98c54c39ad19ca4e4ace4839515f51
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-06 16:16:55 +0900

    [Gold V] Title: 적록색약, Time: 172 ms, Memory: 16680 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,79 +1,72 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 
 public class Main {
+	static boolean[][] visited;
+	static boolean[][] visitedN;
+	static char[][] paint;
+	static int N, areaY, areaN;
+	static int[][] delta = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-
-		char[][] map = new char[N][N];
+		N = Integer.parseInt(br.readLine());
+		visited = new boolean[N][N];
+		visitedN = new boolean[N][N];
+		paint = new char[N][N];
 
 		for (int i = 0; i < N; i++) {
-			map[i] = br.readLine().toCharArray();
+			paint[i] = br.readLine().toCharArray();
 		}
 
-		int[][] delta = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
-		boolean[][] visitedN = new boolean[N][N];
-		boolean[][] visitedY = new boolean[N][N];
-		int cntN = 0, cntY = 0;
-
-		Queue<Integer[]> queue = new ArrayDeque<>();
+		areaY = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (visitedN[i][j] == false) {
-					queue.add(new Integer[] { i, j });
-					cntN++;
-
-					while (!queue.isEmpty()) {
-						for (int[] d : delta) {
-							int tI = queue.peek()[0] + d[0];
-							int tJ = queue.peek()[1] + d[1];
-
-							if (tI > -1 && tI < N && tJ > -1 && tJ < N
-									&& map[tI][tJ] == map[queue.peek()[0]][queue.peek()[1]]
-									&& visitedN[tI][tJ] == false) {
-								queue.add(new Integer[] { tI, tJ });
-								visitedN[tI][tJ] = true;
-							}
-						}
-						queue.poll();
-					}
+				if (!visited[i][j]) {
+					visited[i][j] = true;
+					areaY++;
+					dfsY(i, j, paint[i][j]);
 				}
 			}
 		}
-
-		queue.clear();
+		areaN = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (visitedY[i][j] == false) {
-					queue.add(new Integer[] { i, j });
-					cntY++;
-
-					while (!queue.isEmpty()) {
-						for (int[] d : delta) {
-							int tI = queue.peek()[0] + d[0];
-							int tJ = queue.peek()[1] + d[1];
-
-							if (tI > -1 && tI < N && tJ > -1 && tJ < N && visitedY[tI][tJ] == false) {
-								if (map[tI][tJ] == map[queue.peek()[0]][queue.peek()[1]]
-										|| (map[tI][tJ] == 'G' && map[queue.peek()[0]][queue.peek()[1]] == 'R')
-										|| (map[tI][tJ] == 'R' && map[queue.peek()[0]][queue.peek()[1]] == 'G')) {
-									queue.add(new Integer[] { tI, tJ });
-									visitedY[tI][tJ] = true;
-								}
-
-							}
-						}
-						queue.poll();
-					}
+				if (!visitedN[i][j]) {
+					visitedN[i][j] = true;
+					areaN++;
+					dfsN(i, j, paint[i][j]);
 				}
 			}
 		}
-		
-		System.out.println(cntN+" "+cntY);
+		System.out.println(areaY + " " + areaN);
+	}
 
+	static void dfsY(int curX, int curY, char cur) {
+		for (int[] d : delta) {
+			int newI = curX + d[0];
+			int newJ = curY + d[1];
+			if (newI < 0 || newI >= N || newJ < 0 || newJ >= N || visited[newI][newJ])
+				continue;
+			if (cur == paint[newI][newJ]) {
+				visited[newI][newJ] = true;
+				dfsY(newI, newJ, cur);
+			}
+		}
+
+	}
+
+	static void dfsN(int curX, int curY, char cur) {
+		for (int[] d : delta) {
+			int newI = curX + d[0];
+			int newJ = curY + d[1];
+			if (newI < 0 || newI >= N || newJ < 0 || newJ >= N || visitedN[newI][newJ])
+				continue;
+			if (cur == paint[newI][newJ] || (cur == 'R' && paint[newI][newJ] == 'G')
+					|| (cur == 'G' && paint[newI][newJ] == 'R')) {
+				visitedN[newI][newJ] = true;
+				dfsN(newI, newJ, cur);
+			}
+		}
 	}
 }
