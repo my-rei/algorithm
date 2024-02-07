5d3aba589231dba495900b5a89bd74169fe28f6e
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-07 12:19:06 +0900

    [Silver V] Title: 색종이, Time: 136 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -6,21 +6,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		boolean[][] paper = new boolean[100][100];
+		int c = 0;
 		for (int k = 0; k < N; k++) {
 			String[] color = br.readLine().split(" ");
 
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 10; j++) {
-					paper[Integer.parseInt(color[0])+i][Integer.parseInt(color[1])+j] = true;
+					if(!paper[Integer.parseInt(color[0])+i][Integer.parseInt(color[1])+j]) {
+						c++;
+						paper[Integer.parseInt(color[0])+i][Integer.parseInt(color[1])+j] = true;
+					}
 				}
 			}
 		}
-
-		int c = 0;
-		for (int i = 0; i < 100; i++)
-			for (int j = 0; j < 100; j++)
-				c += paper[i][j] ? 1 : 0;
-
 		System.out.println(c);
 	}
 }
