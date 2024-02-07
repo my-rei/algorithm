57ba85c2cf4a6b82e64f3785d6d097aada63ae1a
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-07 15:16:54 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,9 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
 	// 2147483647 2147483647 100001
+	// 나머지 연산 필요 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] abc = br.readLine().split(" ");
@@ -16,18 +16,12 @@ public class Main {
 
 	static long fastPow(long c, long n, long c2) {
 		long res = 1;
-		int cnt = 0;
 		while (n > 0) {
 			if (n % 2 == 1)
 				res = (res%c2 * c%c2) %c2;
-				//res *= c;
-			//c *= c;
 			c = (c%c2 * c%c2)%c2;
-			//res %= c2;
 			n /= 2;
-//			System.out.println(res+" "+c);
 		}
-//		System.out.println(res + " "+c+" "+cnt);
 		return res;
 	}
 }
