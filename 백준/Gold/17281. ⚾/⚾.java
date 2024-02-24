3a98837c0ccde911cdf070af83bbe0e79cbdfcf4
Yerim <77157624+rime-y@users.noreply.github.com>
2024-02-24 17:18:23 +0900

    [Gold IV] Title: ⚾, Time: 644 ms, Memory: 20012 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -26,6 +26,10 @@ public class Main {
 			}
 		}
 
+		// 4번 타자 고르기
+		player[3] = 0;
+		visited[0] = true;
+
 		makePer(0);
 
 		bw.write(Integer.toString(maxScore));
@@ -34,16 +38,12 @@ public class Main {
 
 	static void makePer(int cnt) {
 		if (cnt == P) {
-//			System.out.println(Arrays.toString(player));
 			maxScore = Math.max(maxScore, getScore());
 			return;
 		}
-
-		if (cnt == 3) {
-			visited[0] = true;
-			player[3] = 0;
+		if (cnt == 3)
 			makePer(cnt + 1);
-		} else {
+		else {
 			for (int i = 0; i < P; i++) {
 				if (!visited[i] && i != 0) {
 					visited[i] = true;
@@ -52,42 +52,35 @@ public class Main {
 					visited[i] = false;
 				}
 			}
-
 		}
+
 	}
 
 	static int getScore() {
 		int res = 0, curN = 0, curOut = 0;
-		int[] pos;
+		int[] pos = new int[8]; // 홈 넘어가는 경우도 포함;
 		int i = 0;
 		while (curN < N) {
-			pos = new int[4];
 			curOut = 0;
-//			System.out.println("=======" + curN + "이닝 시작========");
 			while (curOut < 3) {
-//				System.out.println(i+"번 타자 => "+scores[curN][player[i]]);
 				int curS = scores[curN][player[i]];
 				if (curS == 0) {
 					curOut++;
 				} else {
-					// update pos
-//					System.out.println(Arrays.toString(pos));
-
 					pos[0]++;
 					for (int j = 3; j > -1; j--) {
 						if (pos[j] > 0) {
-							if (j + curS >= 4) {
-								res++;
-								pos[j]--;
-							} else {
-								pos[j + curS]++;
-								pos[j]--;
-							}
+							pos[j + curS]++;
+							pos[j]--;
 						}
 					}
 				}
 				i = (i + 1) % P;
-
+			}
+			// 점수 계산
+			for (int j = 0; j < 8; j++) {
+				if(j > 3) res += pos[j];
+				pos[j] = 0;
 			}
 			curN++;
 		}
