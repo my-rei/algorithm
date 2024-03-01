7fa5e5ad56f084088638184dfc4f955fedd9e2d0
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-01 20:42:41 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 148 ms, Memory: 19532 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M, ri, rj, bi, bj, oi, oj, minTry;
+	static char[][] map;
+	static int[] dr = {0,0,-1,1}, dc = {-1,1,0,0};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new char[N][M];
+		
+		for(int i = 0;i<N;i++) {
+			String str = br.readLine();
+			for(int j =0;j<M;j++) {
+				map[i][j] = str.charAt(j);
+				if(map[i][j] == 'R') { ri = i; rj = j; map[i][j] ='.'; }
+				else if(map[i][j] == 'B') { bi = i; bj = j; map[i][j] = '.';}
+				else if(map[i][j] == 'O'){ oi = i; oj = j; }
+			}
+		}
+		
+		//모든 경우 탐색
+		//각경우마다 이동하여 유효성 판단
+		minTry = Integer.MAX_VALUE;
+		dfs(0, new int[] {0, ri, rj, bi, bj});
+		
+		if(minTry == Integer.MAX_VALUE) minTry = -1;
+		bw.write(String.valueOf(minTry));
+		bw.flush();
+	}
+	
+	static void dfs(int count, int[] cor) {
+		// r, b 좌표로 위치 관리한다 
+		if(count == 10) {
+			return;
+		}
+		
+		if(count > minTry) return;
+		
+		// d 0왼쪽 1오른쪽 2위쪽 3오른쪽 
+		for(int d =0;d<4;d++) {
+			int[] rs = move(d, cor);
+			if(rs[0] == 0) {
+//				System.out.println("["+count+"] "+d+" continue");
+				dfs(count+1, rs);
+			} else if(rs[0] == 1) {
+//				System.out.println("["+count+"] "+d+" success");
+				minTry = Math.min(minTry, count+1);
+			} else if(rs[0] == -1) {
+//				System.out.println("["+count+"] "+d+" fail");
+			}
+		}
+	}
+	
+	static int[] move(int d, int[] cor) {
+		// return -1:실패/움직이지않음 0:이동완료 1:종료 
+		int[] result = Arrays.copyOf(cor, cor.length);
+		boolean flag = false, rStop = false, bStop = false, success = false;
+		int crr = cor[1], crc = cor[2], cbr = cor[3], cbc = cor[4];
+		while(!rStop || !bStop) {
+//			System.out.println(crr+ " "+crc+" "+cbr+" "+cbc+ " "+rStop+" "+bStop+ " "+d);
+			int nrr = crr + dr[d], nrc = crc+dc[d], nbr = cbr+dr[d], nbc = cbc+dc[d];
+			if(map[nrr][nrc] == '#') {nrr = crr; nrc = crc; rStop = true;}
+			if(map[nbr][nbc] == '#') {nbr = cbr; nbc = cbc; bStop = true;}
+			if(!rStop) { //r 전진함
+				if (nrr == nbr && nrc == nbc) {
+					rStop = true;
+				} else if (map[nrr][nrc] == '.') {
+					crr = nrr; crc = nrc;
+					flag = true;
+				} else if(map[nrr][nrc] == '#') {
+					rStop = true;
+				} else if(map[nrr][nrc] == 'O') {
+					nrr = -1; nrc = -1;
+					rStop = true;
+					success = true;
+				}
+			}
+			
+			if(!bStop) { //b 전진함
+				if (nrr == nbr && nrc == nbc && !success) {
+//					System.out.println(nrr+" "+nrc+" 82");
+					bStop = true;
+				} else if (map[nbr][nbc] == '.') {
+//					System.out.println("85");
+					cbr = nbr; cbc = nbc;
+					flag = true;
+				} else if(map[nbr][nbc] == '#') {
+//					System.out.println("dss");
+					bStop = true;
+				} else if(map[nbr][nbc] == 'O') {
+					return new int[] {-1, nrr, nrc, nbr, nbc};
+				}
+			}
+			
+//			crr = nrr; crc = nrc; cbr = nbr; cbc = nbc;
+		}
+		
+		if(success) return new int[] {1, crr, crc, cbr, cbc};
+		int resultcode = flag? 0:-1;
+		return new int[] {resultcode, crr, crc, cbr, cbc};
+	}
+	
+	
+}
