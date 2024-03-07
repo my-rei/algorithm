9ee403635f26544e3c628cf813d0b9a687eaa6a9
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-07 14:46:05 +0900

    [Gold V] Title: 다각형의 면적, Time: 164 ms, Memory: 18096 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		long[][] cor = new long[N][2];
+		for(int i = 0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			cor[i][0] = Long.parseLong(st.nextToken());
+			cor[i][1] = Long.parseLong(st.nextToken());
+		}
+		
+		double res = 0;
+		
+		for(int i = 1;i<N-1;i++) {
+			res += getTri(cor[0][0], cor[0][1], cor[i][0], cor[i][1], cor[i+1][0], cor[i+1][1])/2.0;
+		}
+		//res = (double) Math.round(res*10)/10.0;
+		res = Math.abs(res);
+		BigDecimal bd = new BigDecimal(res);
+		bw.write(String.format("%.1f", res));
+		bw.flush();
+	}
+	
+	static long getTri(long x1, long y1, long x2, long y2, long x3, long y3) {
+		return x1*y2+x2*y3+x3*y1-y1*x2-y2*x3-y3*x1;
+	}
+}
