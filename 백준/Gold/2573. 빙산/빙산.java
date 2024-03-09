3b36b1b1b798c85ae0e991413f51d5445c31af75
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-09 23:46:51 +0900

    [Gold IV] Title: 빙산, Time: 520 ms, Memory: 172164 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 	static int N, M;
 	static int[][] map, newMap;
-	static boolean[][] vs;
 	static Queue<int[]> queue;
 	static int[] dr = {0,1,0,-1}, dc= {1,0,-1,0};
 	public static void main(String[] args) throws Exception {
@@ -26,9 +25,8 @@ public class Main {
 			}
 		}
 		
-		System.out.println(bfs());
-		
-		
+		bw.write(String.valueOf(bfs()));
+		bw.flush();	
 	}
 	
 	static int bfs() {
@@ -57,33 +55,27 @@ public class Main {
 				
 			}
 			
-			map = newMap;	
+			
 			if(checkq.size() == 0) { // 모두 녹음
 				return 0;
 			}
 			int newIceCnt = 0; //임의의 빙하에서 이어진 빙하의 개수 
-			vs = new boolean[N][M];
 			while(!checkq.isEmpty()) {
 				int[] now = checkq.poll();
 				for(int d= 0;d<4;d++) {
 					int nr=now[0]+dr[d], nc=now[1]+dc[d];
 					if(nr<0||nr>=N||nc<0||nc>=M) continue;
-					if(map[nr][nc] == 0 || vs[nr][nc]) continue;
+					if(newMap[nr][nc] == 0 || map[nr][nc] == 0) continue;
 					newIceCnt++;
-					vs[nr][nc] = true;
+					map[nr][nc] = 0;
 					checkq.add(new int[] {nr, nc});
 				}
 			}
-//			for(int i = 0;i<N;i++) {
-//				for(int j = 0;j<M;j++) {
-//					System.out.print(map[i][j]+" ");
-//				}
-//				System.out.println();
-//			}
 			if(newIceCnt != count) {
 				return year;
 			}
 			year++;
+			map = newMap;	
 		}
 		return 0;
 	}
