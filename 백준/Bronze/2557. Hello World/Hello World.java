e0d54f380e77997d9e009e0990ece4d60358c0fa
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-10 17:09:25 +0900

    [Bronze V] Title: Hello World, Time: 72 ms, Memory: 11308 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("Hello World!");
+		bw.flush();	
+	}
+}
