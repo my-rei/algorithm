091c4a6633654b865831d1ce5a4934db7593ae08
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-12 17:40:28 +0900

    [Gold II] Title: 문제집, Time: 440 ms, Memory: 50168 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -2,21 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] preCount;
-	static List<Integer>[] preList;
-	static PriorityQueue<Integer> pq;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		pq = new PriorityQueue<>();
-		preCount = new int[N+1];
-		preList = new List[N+1];
+		int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int[] preCount = new int[N+1];
+		List<Integer>[] preList = new List[N+1];
 		for(int i = 0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());	
 			int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
@@ -25,20 +20,16 @@ public class Main {
 			preList[A].add(B);
 		}
 		
-		for(int i = 1;i<N+1;i++) {
-			if(preCount[i] == 0)
-				pq.add(i);
-		}
-		
+		for(int i = 1;i<N+1;i++) 
+			if(preCount[i] == 0) pq.add(i);
+
 		while(!pq.isEmpty()) {
 			int a = pq.poll();
 			sb.append(a+" ");
 			if(preList[a] == null) continue;
 			for(int b : preList[a]) {
 				preCount[b]--;
-				if(preCount[b] == 0) {
-					pq.add(b);
-				}
+				if(preCount[b] == 0)  pq.add(b);
 			}
 		}
 		bw.write(sb.toString());
