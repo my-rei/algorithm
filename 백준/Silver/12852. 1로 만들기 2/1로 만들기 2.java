2fa743df1c393a2991d55dc29c9110822a77d259
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-12 15:07:02 +0900

    [Silver I] Title: 1로 만들기 2, Time: 748 ms, Memory: 411376 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N<4?4:N+1];
+		List<Integer>[] list = new List[N<4?4:N+1];
+		for(int i = 0;i<4;i++) list[i] = new ArrayList<Integer>();
+		dp[1] = 0; 
+		dp[2] = 1; list[2].add(2);
+		dp[3] = 1; list[3].add(3); 
+		for(int i = 4;i<=N;i++) {
+			dp[i] = dp[i-1]+1;
+			list[i] = new ArrayList<>(list[i-1]);
+			list[i].add(i);
+			if(i%2==0 && dp[i] > dp[i/2]+1) {
+				dp[i] = dp[i/2]+1;
+				list[i] = new ArrayList<>(list[i/2]);
+				list[i].add(i);
+			}
+			if(i%3==0 && dp[i] > dp[i/3]+1) {
+				dp[i] = dp[i/3]+1;
+				list[i] = new ArrayList<>(list[i/3]);
+				list[i].add(i);
+			}
+		}
+		
+		sb.append(dp[N]+"\n");
+		for(int i = list[N].size()-1;i>-1;i--) {
+			sb.append(list[N].get(i)+" ");
+		}
+		sb.append(1);
+		bw.write(sb.toString());
+		bw.flush();
+		
+//		System.out.println(Arrays.toString(dp));
+	}
+}
