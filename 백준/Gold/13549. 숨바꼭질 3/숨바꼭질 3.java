d949deca7751486263bf40fb483c6a74302a860a
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-20 09:09:07 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 16452 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,61 +1,63 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Main {
-	static int N, K, result=Integer.MAX_VALUE;
-	static Set<Integer> set;
-	static int[] dd = {-1,1};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		if(N>=K) result = N-K;
-		else {
-			result = bfs();
-		} 
-		
-		bw.write(String.valueOf(result));
-		bw.flush();
-		
+		String[] nk = br.readLine().split(" ");
+		int N = Integer.parseInt(nk[0]), K = Integer.parseInt(nk[1]);
+
+		System.out.println(bfs(N, K));
+
 	}
-	
-	static int bfs() {
-		Queue<int[]> q= new ArrayDeque<>();
-		set = new HashSet<>();
-		int s = K-N; 
-		q.add(new int[] {N,0});
-		
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-//			System.out.println(Arrays.toString(cur));
-			
-			for(int n =cur[0];;n*=2) {
-//				System.out.println(n);
-				if(n > s+K || n > 100000 || n == 0) break;
-				if(set.contains(n)) continue;
-				if(n == K) return cur[1];
-				set.add(n);
-				q.add(new int[] {n,cur[1]});
-			}
-			
-			
-			for(int d=0;d<dd.length;d++) {
-				int nc = cur[0]+dd[d];
-				if(nc>-1 && nc<100001 && !set.contains(nc)) {
-					if(nc == K) return cur[1]+1;
-					set.add(nc);
-					q.add(new int[] {nc,cur[1]+1});
+
+	static int bfs(int N, int K) {
+		Queue<int[]> queueW = new ArrayDeque<>();
+		Queue<int[]> queueJ = new ArrayDeque<>();
+		boolean[] visited = new boolean[100001];
+
+		if (N == K)
+			return 0;
+		else if (N > K)
+			return N - K;
+		else {
+			visited[N] = true;
+			queueW.add(new int[] { N, 0 });
+			while (!queueW.isEmpty()) {
+				int cur = queueW.peek()[0];
+				int curT = queueW.peek()[1];
+				//System.out.println(cur);
+				queueJ.add(queueW.poll());
+
+				while (!queueJ.isEmpty()) {
+					int jCur = queueJ.peek()[0];
+					int jCurT = queueJ.peek()[1];
+					queueJ.poll();
+
+					int nextCur = jCur * 2;
+					if (nextCur == K)
+						return jCurT;
+					if (nextCur < K * 2 && nextCur < 100001 && !visited[nextCur]) {
+						visited[nextCur] = true;
+						queueJ.add(new int[] { nextCur, jCurT });
+						queueW.add(new int[] {nextCur, jCurT});
+					}
+				}
+
+				int[] delta = new int[] { cur - 1, cur + 1 };
+				for (int d : delta) {
+					if (d > -1 && d < 100001 && !visited[d]) {
+						if (d == K)
+							return curT + 1;
+						visited[d] = true;
+						queueW.add(new int[] { d, curT + 1 });
+					}
 				}
 			}
-			
 		}
-		
-		return result;
-		
+
+		return -1;
 	}
 }
