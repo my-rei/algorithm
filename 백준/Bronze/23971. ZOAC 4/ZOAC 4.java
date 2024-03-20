42dd1a752148d6ea59c9fe9e880dc9b74c5be693
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-20 17:29:47 +0900

    [Bronze III] Title: ZOAC 4, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -5,12 +5,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int H = Integer.parseInt(st.nextToken()), W = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken()), M =Integer.parseInt(st.nextToken());
-		int oneHeight = N+1, oneWidth = M+1;
-		int cntH = H%(N+1) > 0? H/(N+1)+1:H/(N+1), cntW =W/(M+1) + (W%(M+1) > 0? 1:0);
+		int H = Integer.parseInt(st.nextToken()), W = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken())+1, M =Integer.parseInt(st.nextToken())+1;
+		int cntH = H/N +(H%N == 0?0:1), cntW = W/M + (W%M==0?0:1);
 		bw.write(String.valueOf(cntH*cntW));
 		bw.flush();
 	}
