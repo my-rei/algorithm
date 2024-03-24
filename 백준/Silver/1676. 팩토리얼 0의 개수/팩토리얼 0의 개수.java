deb6965ed1c37835fba07b3c25ac4f4ba2ecb004
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-24 20:35:33 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 80 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -5,14 +5,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine()), cnt = 0;
-		int ten = 0, two = 0, five= 0;
+		int N = Integer.parseInt(br.readLine());
+		int two = 0, five= 0;
 		for(int i = N;i>0;i--) {
 			int tmp = i;
-			while(tmp%10 == 0) {
-				tmp /= 10;
-				ten++;
-			}
 			while(tmp%2 == 0) {
 				tmp /= 2;
 				two++;
@@ -22,7 +18,7 @@ public class Main {
 				five++;
 			}
 		}
-		bw.write(String.valueOf(ten+Math.min(two, five)));
+		bw.write(String.valueOf(Math.min(two, five)));
 		bw.flush();
 	}
 	
