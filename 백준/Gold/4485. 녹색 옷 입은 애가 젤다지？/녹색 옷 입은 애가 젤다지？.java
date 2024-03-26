070549552888d07948dc9cf61063a083187a656d
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-26 11:46:55 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 684 ms, Memory: 266088 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,49 @@
+import java.io.*; 
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int[][] map, dis;
+	static int[] dr = {1,0,-1,0}, dc= {0,1,0,-1};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		int t = 1;
+		
+		while((N=Integer.parseInt(br.readLine())) != 0) {
+			map = new int[N][N];
+			dis = new int[N][N];
+			for(int i = 0;i<N;i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0;j<N;j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					dis[i][j] = -1;
+				}
+			}
+			dijkstra();
+			sb.append("Problem "+(t++)+": "+dis[N-1][N-1]+"\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	
+	static void dijkstra() {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] {0,0, map[0][0]});
+		
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			for(int d=0;d<4;d++) {
+				int nr = now[0]+dr[d], nc = now[1]+dc[d];
+				if(nr<0||nr>=N||nc<0||nc>=N) continue;
+				if(dis[nr][nc] == -1 || now[2] + map[nr][nc] < dis[nr][nc]) {
+					dis[nr][nc] = now[2]+map[nr][nc];
+					queue.add(new int[] {nr, nc, dis[nr][nc]});
+				}
+			}
+		}
+	}
+}
