8559e7c26ef1bb12fa13afa96e9891874adb937a
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-26 09:52:08 +0900

    [Silver II] Title: 연속합, Time: 204 ms, Memory: 22088 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N];
+		for(int i = 0;i<N;i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		long[] dp = new long[N];
+		dp[0] = nums[0]; long maxSum = nums[0];
+		for(int i = 1;i<N;i++) {
+			dp[i] = Math.max(nums[i]+dp[i-1], nums[i]);
+			maxSum = Math.max(dp[i], maxSum);
+		}
+		bw.write(String.valueOf(maxSum));
+		bw.flush();
+	}
+}
