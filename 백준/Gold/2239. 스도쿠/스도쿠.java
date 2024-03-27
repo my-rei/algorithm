24c7958002243d6b505f5f1e95f4a3d1b20b665c
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-27 15:30:14 +0900

    [Gold IV] Title: 스도쿠, Time: 376 ms, Memory: 13724 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,9 +1,8 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
 	static final int N=9;
-	static boolean flag;
+	static boolean flag=false;
 	static int[] check;
 	static int[][] puzzle;
 	static StringBuilder sb;
@@ -19,7 +18,7 @@ public class Main {
 				puzzle[i][j] = str.charAt(j) - '0';
 			}
 		}
-		flag = false;
+
 		dfs(0);
 		bw.write(sb.toString());
 		bw.flush();
@@ -40,15 +39,16 @@ public class Main {
 		int r = cor/N, c = cor%N;
 		if(puzzle[r][c] != 0) { dfs(cor+1); }
 		else {
-			boolean[] chk = new boolean[N+1];
 			int offsetR = (r/3)*3, offsetC = (c/3)*3;
+			int ch = 0;
 			for(int i = 0;i<N;i++) {
-				chk[puzzle[r][i]] = true;
-				chk[puzzle[i][c]] = true;
-				chk[puzzle[offsetR+i/3][offsetC+i%3]] = true;
+				ch |= 1 << puzzle[r][i]; 
+				ch |= 1 << puzzle[i][c]; 
+				ch |= 1 << puzzle[offsetR+i/3][offsetC+i%3]; 
 			}
+			
 			for(int n = 1;n<=9;n++) {
-				if(!chk[n]) {
+				if((ch>>n & 1) == 0) {
 					puzzle[r][c] = n;
 					dfs(cor+1);
 					puzzle[r][c] = 0;
@@ -56,6 +56,4 @@ public class Main {
 			}
 		}
 	}
-	
-	
 }
