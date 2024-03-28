0c743ff46704bc52325af7cd93ea688258735c37
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-28 10:12:12 +0900

    [Gold IV] Title: 스도쿠, Time: 416 ms, Memory: 16844 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -10,9 +10,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		sb = new StringBuilder();
-		puzzle = new int[N][N];
 		
-		// 입력받는 부분
+		puzzle = new int[N][N];
 		for(int i = 0;i<N;i++) {
 			String str = br.readLine();
 			for(int j = 0;j<N;j++) {
@@ -20,10 +19,7 @@ public class Main {
 			}
 		}
 
-		// 로직 실행 
 		dfs(0);
-		
-		// 출력하는 부분
 		bw.write(sb.toString());
 		bw.flush();
 	}
@@ -31,23 +27,24 @@ public class Main {
 	static void dfs(int cor) {
 		if(flag) return;
 		if(cor == 81) {
+			flag = true;
 			for(int i = 0;i<N;i++) {
 				for(int j = 0;j<N;j++) {
 					sb.append(puzzle[i][j]);
 				}
 				sb.append("\n");
 			}
-			flag = true;
 			return;
 		}
 		int r = cor/N, c = cor%N;
 		if(puzzle[r][c] != 0) { dfs(cor+1); }
 		else {
+			int offsetR = (r/3)*3, offsetC = (c/3)*3;
 			int ch = 0;
 			for(int i = 0;i<N;i++) {
 				ch |= 1 << puzzle[r][i]; 
 				ch |= 1 << puzzle[i][c]; 
-				ch |= 1 << puzzle[(r/3)*3+i/3][(c/3)*3+i%3]; 
+				ch |= 1 << puzzle[offsetR+i/3][offsetC+i%3]; 
 			}
 			
 			for(int n = 1;n<=9;n++) {
