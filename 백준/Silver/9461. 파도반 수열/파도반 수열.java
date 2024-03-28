ad1331bd172dba4af6e1735dc8e2d02fef24bf4c
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-28 17:50:31 +0900

    [Silver III] Title: 파도반 수열, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,15 +1,13 @@
 import java.io.*;
-import java.util.*;
+
 public class Main {
-	static int last = 3;
-	static long[] lines;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine());
-		lines = new long[101];
+		int T = Integer.parseInt(br.readLine()), last = 3;
+		long[] lines = new long[101];
 		lines[0] = 0;
 		lines[1] = 1;
 		lines[2] = 1;
@@ -17,9 +15,8 @@ public class Main {
 		while(T-->0) {
 			int N = Integer.parseInt(br.readLine());
 			if(lines[N] == 0) {
-				for(int i = last;i<=N;i++) {
+				for(int i = last;i<=N;i++) 
 					lines[i] = lines[i-2]+lines[i-3];
-				}
 				last = N;
 			}
 			sb.append(lines[N]+"\n");
