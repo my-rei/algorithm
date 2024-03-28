b34532ca9fed5b887f719f40f87542121cabd6a1
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-28 17:15:33 +0900

    [Silver IV] Title: 수 찾기, Time: 504 ms, Memory: 50712 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -20,10 +20,9 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0;i<M;i++) {
 			int t = Integer.parseInt(st.nextToken());
-			sb.append(t < nums[0] || half(t) >= N? "0":"1").append("\n");
+			sb.append(half(t) >= N? "0":"1").append("\n");
 		}
 		
-//		System.out.println(half);
 		bw.write(sb.toString());
 		bw.flush();
 	}
