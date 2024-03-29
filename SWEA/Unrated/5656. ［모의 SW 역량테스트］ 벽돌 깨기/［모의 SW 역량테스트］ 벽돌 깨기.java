9b6cc74c6b1ce04a68940181d4f03f83d15c77ca
Yerim <77157624+rime-y@users.noreply.github.com>
2024-03-29 11:38:19 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 314 ms, Memory: 106,984 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Solution {
     static int N, W, H, minCount, total;
     static int[] topIndex;
-    static int[][] map;
+    static char[][] map;
     static int[] dr = {1, 0, -1, 0}, dc  = {0,1,0,-1};
-    static Queue<Integer> cQueue;
+    static Queue<Character> cQueue;
     static Queue<int[]> iQueue;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,16 +23,10 @@ public class Solution {
             W = Integer.parseInt(st.nextToken());
             H = Integer.parseInt(st.nextToken());
              
-            map = new int[H][W];
+            map = new char[H][W];
             topIndex = new int[W];
-            for(int i = 0;i<H;i++) {
-            	st = new StringTokenizer(br.readLine());
-            	for(int j = 0;j<W;j++) {
-            		map[i][j] = Integer.parseInt(st.nextToken());
-            	}
-            }
-            
-            
+            for(int i = 0;i<H;i++)
+                map[i] = br.readLine().replace(" ", "").toCharArray();
  
             total = countB();
             minCount = Integer.MAX_VALUE;
@@ -50,14 +44,14 @@ public class Solution {
         }
          
         for(int i = 0;i<W;i++) {
-        	int[][] cMap = deepCopy(map);
+            char[][] cMap = deepCopy(map);
             dfs(cnt+1, broken+drop(i));
             map=cMap;
         }
     }
      
-    static int[][] deepCopy(int[][] target){
-    	int[][] newMap = new int[H][W];
+    static char[][] deepCopy(char[][] target){
+        char[][] newMap = new char[H][W];
         for(int i = 0;i<H;i++) 
             newMap[i] = Arrays.copyOf(target[i], W);
         return newMap;
@@ -67,7 +61,7 @@ public class Solution {
         int count = 0;
         for(int i = 0;i<H;i++) 
             for(int j = 0;j<W;j++) 
-                if(map[i][j] != 0)
+                if(map[i][j] != '0')
                     count++;
         return count;
     }
@@ -81,8 +75,8 @@ public class Solution {
             // 블럭 없음
             return 0;
         }
-        iQueue.add(new int[] {topIndex[wi], wi, map[topIndex[wi]][wi]});
-        map[topIndex[wi]][wi] = 0;
+        iQueue.add(new int[] {topIndex[wi], wi, map[topIndex[wi]][wi]-'0'});
+        map[topIndex[wi]][wi] = '0';
         count++;
          
         while(!iQueue.isEmpty()) {
@@ -93,9 +87,9 @@ public class Solution {
                 for(int l = 1;l<level;l++) {
                     cr += dr[d]; cc += dc[d];
                     if(cr<0||cr>=H||cc<0||cc>=W) break;
-                    if(map[cr][cc] == 0) continue;
-                    if(map[cr][cc] != 1) iQueue.add(new int[] {cr, cc, map[cr][cc]});
-                    map[cr][cc] = 0;
+                    if(map[cr][cc] == '0') continue;
+                    if(map[cr][cc] != '1') iQueue.add(new int[] {cr, cc, map[cr][cc]-'0'});
+                    map[cr][cc] = '0';
                     count++;
                 }
             }
@@ -108,19 +102,18 @@ public class Solution {
     }
      
     static void down() {
-    	int[][] newMap = new int[H][W];
         for(int j = 0;j<W;j++) {
             cQueue.clear();
             for(int i = H-1;i>-1;i--) {
-                if(map[i][j] != 0)
+                if(map[i][j] != '0')
                     cQueue.add(map[i][j]);
+                map[i][j] = '0';
             }
             int count = H;
             while(!cQueue.isEmpty()) {
-                newMap[--count][j] = cQueue.poll();
+                map[--count][j] = cQueue.poll();
             }
             topIndex[j] = count;
         }
-        map = newMap;
     }
 }
