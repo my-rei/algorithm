f9f7868708c2535531a4000f94c16b1648ecc692
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-03 15:40:45 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,169 ms, Memory: 100,380 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,18 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int N,M;
-	static boolean[][] table;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
 		
 		int T = Integer.parseInt(br.readLine());
 		for(int test = 1;test<=T;test++) {
-			N = Integer.parseInt(br.readLine());
-			M = Integer.parseInt(br.readLine());
-			table = new boolean[N+1][N+1];
+			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+			boolean[][] table = new boolean[N+1][N+1];
 			for(int i = 0;i<M;i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken()), b= Integer.parseInt(st.nextToken());
@@ -33,26 +32,34 @@ public class Solution {
 			}
 			
 			int cnt = 0;
-			for(int i = 1;i<=N;i++) 
-				cnt += count(i);
+			for(int t = 1;t<=N;t++) {
+				int s= 1;
+				for(int i = 1;i<=N;i++) {
+					if(i != t && !table[i][t] && !table[t][i]) {
+						s= 0; break;
+					}
+				}
+				cnt += s;
+			}
+				
 			
 //			sb.append("#"+test+" "+cnt+"\n");
-//			sb.append("#").append(test).append(" ").append(cnt).append("\n");
-			bw.append("#").append(String.valueOf(test)).append(" ").append(String.valueOf(cnt)).append("\n");
+			sb.append("#").append(test).append(" ").append(cnt).append("\n");
+//			bw.append("#").append(String.valueOf(test)).append(" ").append(String.valueOf(cnt)).append("\n");
 		}
-//		bw.write(sb.toString());
+		bw.write(sb.toString());
 		bw.flush();
 	}
 	
-	static int count(int t) {
-		for(int i = 1;i<=N;i++) {
-			if(i != t && !table[i][t] && !table[t][i]) {
-				return 0;
-			}
-//			r++;
-//			if(table[i][t]) r++;
-//			if(table[t][i]) r++;
-		}
-		return 1;
-	}
+//	static int count(int t) {
+//		for(int i = 1;i<=N;i++) {
+//			if(i != t && !table[i][t] && !table[t][i]) {
+//				return 0;
+//			}
+////			r++;
+////			if(table[i][t]) r++;
+////			if(table[t][i]) r++;
+//		}
+//		return 1;
+//	}
 }
