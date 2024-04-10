9a5136bb45a8fcf7bf8c951596dbd571bc8292ea
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-10 14:58:07 +0900

    [Gold IV] Title: 구슬 찾기, Time: 152 ms, Memory: 13488 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static boolean[][] gt;
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		gt = new boolean[N][N];
+		for(int i = 0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken())-1, b = Integer.parseInt(st.nextToken())-1;
+			gt[a][b] = true;
+//			gt[b][a] = true;
+		}
+		
+		for(int k = 0;k<N;k++) {
+			for(int i = 0;i<N;i++) {
+//				if(i == k || !gt[i][k]&&!gt[k][i]) continue;
+				for(int j = 0;j<N;j++) {
+					gt[i][j] |= gt[i][k]&&gt[k][j];
+				}
+			}
+		}
+		
+//		for(int i = 0;i<N;i++) {
+//			for(int j = 0;j<N;j++) {
+//				System.out.print(gt[i][j]? "1":"0");
+//			}
+//			System.out.println();
+//		}
+		
+		int res = 0;
+		for(int i = 0;i<N;i++) {
+			int lc = 0, gc = 0; 
+			for(int j = 0;j<N;j++) {
+				lc += gt[i][j]? 1:0;
+				gc += gt[j][i]? 1:0;
+			}
+			res += lc >= (N+1)/2||gc>=(N+1)/2? 1:0;
+		}
+		bw.write(String.valueOf(res));
+		bw.flush();
+	}
+
+}
