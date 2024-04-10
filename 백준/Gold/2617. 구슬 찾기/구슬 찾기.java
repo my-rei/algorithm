2f392e73725a7b990b970181b12134c6909f552a
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-10 15:00:58 +0900

    [Gold IV] Title: 구슬 찾기, Time: 132 ms, Memory: 13768 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
@@ -2,18 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static boolean[][] gt;
-	
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		gt = new boolean[N][N];
+		boolean[][] gt = new boolean[N][N];
 		for(int i = 0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken())-1, b = Integer.parseInt(st.nextToken())-1;
