6eab190635acd97e7de30a2b5049494c8d61ea70
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-11 13:50:15 +0900

    [Gold I] Title: 구슬 탈출, Time: 92 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
@@ -2,101 +2,92 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static boolean find;
-	static int N, M;
-	static char[][] board;
-	static int[] dr = {0,1,0,-1}, dc = {1,0,-1,0};
-	public static void main(String[] args) throws IOException {
+	static boolean flag;
+	static int N, M, ri, rj, bi, bj, oi, oj;
+	static char[][] map;
+	static int[] dr = {0,0,-1,1}, dc = {-1,1,0,0};
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-		board = new char[N][M];
-		for(int i = 0;i<N;i++)
-			board[i] = br.readLine().toCharArray();
-		dfs(0, board);
-		bw.write(find? "1":"0");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new char[N][M];
+		
+		for(int i = 0;i<N;i++) {
+			String str = br.readLine();
+			for(int j =0;j<M;j++) {
+				map[i][j] = str.charAt(j);
+				if(map[i][j] == 'R') { ri = i; rj = j; map[i][j] ='.'; }
+				else if(map[i][j] == 'B') { bi = i; bj = j; map[i][j] = '.';}
+				else if(map[i][j] == 'O'){ oi = i; oj = j; }
+			}
+		}
+		
+		//모든 경우 탐색
+		//각경우마다 이동하여 유효성 판단
+//		minTry = Integer.MAX_VALUE;
+		dfs(0, new int[] {0, ri, rj, bi, bj});
+		
+//		if(minTry == Integer.MAX_VALUE) minTry = -1;
+		bw.write(flag? "1":"0");
 		bw.flush();
 	}
 	
-	static void dfs(int count, char[][] map) {
-		if(find || count == 10) return;
-		int rr=0,rc=0,br=0,bc=0;
-		for(int i = 0;i<N;i++) {
-			for(int j = 0;j<M;j++) {
-				if(map[i][j] == 'R') {
-					rr = i; rc = j;
-				} else if(map[i][j] == 'B') {
-					br=i;bc=j;
+	static void dfs(int count, int[] cor) {
+		// r, b 좌표로 위치 관리한다 
+		if(count == 10 || flag)  return;
+//		if(count > minTry) return;
+		
+		// d 0왼쪽 1오른쪽 2위쪽 3오른쪽 
+		for(int d = 0;d<4;d++) {
+			int[] rs = move(d, cor);
+			if(rs[0] == 0) {
+				dfs(count+1, rs);
+			} else if(rs[0] == 1) {
+				flag = true;
+//				minTry = Math.min(minTry, count+1);
+                return;
+			}
+		}
+	}
+	
+	static int[] move(int d, int[] cor) {
+		// return -1:실패/움직이지않음 0:이동완료 1:종료 
+		boolean flag = false, rStop = false, bStop = false, success = false;
+		int crr = cor[1], crc = cor[2], cbr = cor[3], cbc = cor[4];
+		while(!rStop || !bStop) {
+			int nrr = crr + dr[d], nrc = crc+dc[d], nbr = cbr+dr[d], nbc = cbc+dc[d];
+			if(map[nrr][nrc] == '#') {nrr = crr; nrc = crc; rStop = true;}
+			if(map[nbr][nbc] == '#') {nbr = cbr; nbc = cbc; bStop = true;}
+			if(!rStop) { //r 전진함
+				if (nrr == nbr && nrc == nbc) {
+					rStop = true;
+				} else if (map[nrr][nrc] == '.') {
+					crr = nrr; crc = nrc;
+					flag = true;
+				} else if(map[nrr][nrc] == 'O') {
+					rStop = true;
+					success = true;
+				}
+			}
+			
+			if(!bStop) { //b 전진함
+				if (nrr == nbr && nrc == nbc && !success) {
+					bStop = true;
+				} else if (map[nbr][nbc] == '.') {
+					cbr = nbr; cbc = nbc;
+					flag = true;
+				} else if(map[nbr][nbc] == 'O') {
+					return new int[] {-1, nrr, nrc, nbr, nbc};
 				}
 			}
 		}
-		for(int d = 0;d<4;d++) {
-			char[][] newMap = copy(map);
-			if(notFail(newMap, d, rr, rc, br, bc)) {
-				//System.out.println("======== dir: "+d+" =======");
-				//p(newMap);
-				dfs(count+1, newMap);
-			}
-		}
-	}
-	
-	static char[][] copy(char[][] map){
-		char[][] res = new char[N][M];
-		for(int i = 0;i<N;i++) {
-			res[i] = Arrays.copyOf(map[i], M);
-		}
-		return res;
-	}
-	static void p(char[][] map) {
-		for(int i = 0;i<N;i++) {
-			for(int j = 0;j<M;j++) {
-				System.out.print(map[i][j]);
-			}
-			System.out.println();
-		}
-	}
-	
-	static boolean notFail(char[][] map, int dir, int rr, int rc, int br, int bc) {
-		map[rr][rc] = '.';
-		while(true) {
-			int nr = rr + dr[dir], nc = rc+dc[dir];
-			if(map[nr][nc] == 'O') {
-				rr = nr; rc = nc; break;
-			}
-			if(map[nr][nc] != '.') break;
-			rr = nr; rc = nc;
-		}
-		map[rr][rc] = 'R';
 		
-		map[br][bc] = '.';
-		while(true) {
-			int nr = br + dr[dir], nc = bc+dc[dir];
-//			System.out.println("b "+nr+" "+nc);
-			if(board[nr][nc] == 'O') return false;
-			if(map[nr][nc] != '.') break;
-			br = nr; bc = nc;
-		}
-		map[br][bc] = 'B';
-		
-		map[rr][rc] = '.';
-		int nr = rr, nc = rc;
-		while(true) {
-			if(board[nr][nc] == 'O') {
-				find = true;
-				return true;
-			}
-			if(map[nr][nc] != '.') break;
-			rr = nr; rc = nc;
-			nr += dr[dir]; nc += dc[dir];
-		}
-		map[rr][rc] = 'R';
-		
-		
-		return true;
+		if(success) return new int[] {1, crr, crc, cbr, cbc};
+		int resultcode = flag? 0:-1;
+		return new int[] {resultcode, crr, crc, cbr, cbc};
 	}
-
-	
-	
 }
