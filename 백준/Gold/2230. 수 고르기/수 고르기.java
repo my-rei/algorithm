66a4ef5feb09ff397d9d5490f0a38ff11c420994
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-15 14:11:03 +0900

    [Gold V] Title: 수 고르기, Time: 2408 ms, Memory: 27460 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+		
+		int[] nums = new int[N];
+		for(int i = 0;i<N;i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(nums);
+		
+		int s = 0, e = 1; long dif = Long.MAX_VALUE;
+		while(s<e && e<N) {
+			if(nums[e]-nums[s] == M) {
+				dif = M;
+				break;
+			} else if (nums[e] - nums[s] > M) {
+				dif = Math.min(dif, nums[e]-nums[s]);
+				s++; e = s+1;
+			} else {
+				e++;
+			}
+		}
+				
+				
+		bw.write(String.valueOf(dif));
+		bw.flush();
+	}
+}
