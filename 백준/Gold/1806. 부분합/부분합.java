c55731c0b69541a78e91a5aed90d522616638874
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-15 15:17:01 +0900

    [Gold IV] Title: 부분합, Time: 212 ms, Memory: 23568 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,35 +1,36 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+	static int N, S;
+	static int[] nums, sums;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken()), S = Integer.parseInt(st.nextToken());
-		int[] nums = new int[N];
+		N = Integer.parseInt(st.nextToken()); S = Integer.parseInt(st.nextToken());
+		nums = new int[N]; sums = new int[N+1];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0;i<N;i++) {
+		nums[0] = Integer.parseInt(st.nextToken());
+		for(int i=1;i<N;i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
+			sums[i] = sums[i-1]+nums[i-1];
 		}
-		
-		int left = 0, right = 0, length = Integer.MAX_VALUE, tmp = nums[0];
-		while(true) {
-			if(tmp >= S) {
-				length = Math.min(length, right-left+1);
-				tmp -= nums[left];
-				left++;
-				if(left > right) break;
-			} else {
-				right++;
-				if(right == N)  break;
-				tmp += nums[right];	
+		sums[N] = sums[N-1]+nums[N-1];
+		int minL = Integer.MAX_VALUE, e = 0;
+		for(int i = 0;i<N;i++) {
+			while(e<N) {
+				if(sums[e+1]-sums[i] >= S) {
+					minL = Math.min(minL, e-i+1);
+					break;
+				}
+				e++;
 			}
 		}
-		
-		bw.write(String.valueOf(length==Integer.MAX_VALUE? 0:length));
+		bw.write(String.valueOf(minL==Integer.MAX_VALUE? 0:minL));
 		bw.flush();
 	}
+	
 }
