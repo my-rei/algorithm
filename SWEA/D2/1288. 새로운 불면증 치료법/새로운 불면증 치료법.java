318ce3312b329c39c307ee205b565f5a18b498c1
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-20 10:53:55 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 111 ms, Memory: 22,484 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		int bit = (1<<10)-1;
+		for(int t = 1;t<=T;t++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			int cur = 0, num = 0, level = 0;
+			while(cur != bit) {
+				level++; num += N; 
+//				System.out.println("cur =["+Integer.toBinaryString(cur)+"]");
+				int tmp = num;
+				while(tmp > 0) {
+					cur |= 1 << (tmp % 10);
+					tmp /= 10;
+				}
+			}
+			sb.append("#").append(t).append(" ").append(num).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
