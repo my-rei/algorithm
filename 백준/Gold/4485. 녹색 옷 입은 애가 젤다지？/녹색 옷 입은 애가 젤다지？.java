8c9f256e8bc1f484e65d22ccaa5537138dc80408
Yerim <77157624+rime-y@users.noreply.github.com>
2024-04-21 23:23:37 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -31,18 +31,24 @@ public class Main {
 	}
 	
 	static void dijkstra() {
-		Queue<int[]> queue = new ArrayDeque<>();
-		queue.add(new int[] {0,0});
+//		Queue<int[]> queue = new ArrayDeque<>();
+		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(dis[o1[0]][o1[1]],dis[o2[0]][o2[1]]);
+			}
+		});
+		pq.add(new int[] {0,0});
 		dis[0][0] = map[0][0];
 		
-		while(!queue.isEmpty()) {
-			int[] now = queue.poll();
+		while(!pq.isEmpty()) {
+			int[] now = pq.poll();
 			for(int d=0;d<4;d++) {
 				int nr = now[0]+dr[d], nc = now[1]+dc[d];
 				if(nr<0||nr>=N||nc<0||nc>=N) continue;
 				if(dis[nr][nc] == -1 || dis[now[0]][now[1]] + map[nr][nc] < dis[nr][nc]) {
 					dis[nr][nc] = dis[now[0]][now[1]]+map[nr][nc];
-					queue.add(new int[] {nr, nc});
+					pq.add(new int[] {nr, nc});
 				}
 			}
 		}
