b1210bb2a5f575a4795161a9037ef339871a42b8
Yerim <77157624+rime-y@users.noreply.github.com>
2024-05-29 00:44:17 +0900

    [Silver III] Title: 수열, Time: 212 ms, Memory: 22168 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -18,13 +13,13 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		int[] arr = new int[N];
-		for(int i = 0;i<N;i++) {
+		for(int i = 0;i<N;i++) 
 			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		
 		int sum = 0;
-		for(int i = 0;i<K;i++) {
+		for(int i = 0;i<K;i++) 
 			sum += arr[i];
-		}
+		
 		int maxSum = sum;
 		for(int i = K;i<N;i++) {
 			sum = sum - arr[i-K] + arr[i];
