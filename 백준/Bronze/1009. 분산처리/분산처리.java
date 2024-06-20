8f2090869ac49dcf23dc67e8776a27c639f1158b
Yerim <77157624+rime-y@users.noreply.github.com>
2024-06-21 01:01:23 +0900

    [Bronze II] Title: 분산처리, Time: 80 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -11,11 +11,11 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken()) % 10;
 			int b = Integer.parseInt(st.nextToken());
-			double res = 0;
-			if(a == 1 || a == 5 || a==6) res = a;
-			else if (a == 0) res = 10;
+			double res = a;
+
+			if (a == 0) res = 10;
 			else if(a==4 || a == 9) res = Math.pow(a, b%2+2) % 10;
-			else res = Math.pow(a, b%4+4) % 10;
+			else if(a==2 || a==3 || a==7 || a==8) res = Math.pow(a, b%4+4) % 10;
 
 			sb.append((int) res).append("\n");
 		}
