491f5d2db606682a4234b448ffa4082b253628c9
Yerim <77157624+rime-y@users.noreply.github.com>
2024-07-09 22:21:15 +0900

    [Bronze III] Title: Fastestest Function, Time: 72 ms, Memory: 11652 KB -BaekjoonHub

---
+++ 백준/Bronze/25985. Fastestest Function/Fastestest Function.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		bw.write(String.valueOf((double) (x*(100-y)) / (y*(100-x))));	
+		bw.flush();
+	}
+}
