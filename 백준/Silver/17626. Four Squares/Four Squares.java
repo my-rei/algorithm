2ab48839eb69df2f88dd140fa04becf120db118b
Yerim <77157624+rime-y@users.noreply.github.com>
2024-07-10 09:11:17 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,35 +1,43 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int cnt=0;
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		
+		int N = Integer.parseInt(br.readLine());
 		int[] dp = new int[50001];
+		
 		dp[1] = 1;
 		dp[2] = 2;
 		dp[3] = 3;
 		dp[4] = 1;
-		for(int i = 5;i<=n;i++) {
-			if(isSqrt(i)) {dp[i] = 1; continue;}
-			int t = (int) Math.sqrt(i);
-			int minC = 5;
-			for(int k = t;k>0;k--) {
-				
-				if (dp[(int)Math.pow(k, 2)] != 0 || dp[i - (int)Math.pow(k, 2)] != 0) {
-					minC = Math.min(dp[(int)Math.pow(k, 2)]+dp[i - (int)Math.pow(k, 2)], minC);
-					}
+		
+		for(int i = 5;i<=N;i++) {
+			if(Math.pow((int)Math.sqrt(i), 2) == i) {
+				dp[i] = 1;
+				continue;
 			}
-			dp[i] = minC;
+			dp[i] = 5;
+			for(int j = (int)Math.sqrt(i);j>0;j--) {
+				int t = (int)Math.pow(j,2);
+				dp[i] = Math.min(dp[t]+dp[i-t], dp[i]);
+			}
 		}
-		System.out.println(dp[n]);
+		
+
+		bw.write(String.valueOf(dp[N]));
+		bw.flush();
 	}
-	static boolean isSqrt(int n) {
-		return Math.pow((int)Math.sqrt(n), 2) == n;
-	}
+
 }
