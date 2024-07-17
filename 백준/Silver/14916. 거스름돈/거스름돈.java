0ec3afec031d3ce18cec4b72ada69a3143c328a0
Yerim <77157624+rime-y@users.noreply.github.com>
2024-07-17 09:11:36 +0900

    [Silver V] Title: 거스름돈, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -4,13 +4,11 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int money = Integer.parseInt(br.readLine());
 		int res = -1;
 		if(money != 1 && money != 3) 
 			res = money%5%2==0? money/5+(money%5/2):(money/5-1)+(money%5+5)/2;
-		bw.write(String.valueOf(res));
-		bw.flush();
+		System.out.println(res);
 	}
 
 }
