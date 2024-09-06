7a4ab071ab9aacf40e0d9a8f4b42ce82658e5ebe
Yerim <77157624+rime-y@users.noreply.github.com>
2024-09-06 16:51:03 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 64 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -1,12 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -18,54 +11,35 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
-		int[] count = new int[K+1];
 		Queue<Integer>[] list = new Queue[K+1];
 		int[] seq = new int[K];
 		for(int i = 0;i<K;i++) {
 			int c = Integer.parseInt(st.nextToken());
 			seq[i] = c;
-			count[c]++;
 			if(list[c] == null) list[c] = new ArrayDeque<>();
 			list[c].add(i);
 		}
 		int cnt = 0;
 		List<Integer> tab = new ArrayList<>();
 		for(int i = 0;i<K;i++) {
-//			System.out.println("start "+tab.toString());
 			if(!tab.contains(seq[i])) {
 				if(tab.size() < N) {
 					tab.add(seq[i]);
 				} else {
-					int minV = K, minI = 0, minC = 0;
+					int minV = -1, minI = 0;
 					for(int j = 0;j<tab.size();j++) {
-						if(minV > count[tab.get(j)]) {
-							minV = count[tab.get(j)];
+						int last = list[tab.get(j)] == null || list[tab.get(j)].isEmpty()? Integer.MAX_VALUE:list[tab.get(j)].peek();
+						if(minV < last) {
+							minV = last;
 							minI = j;
-							minC = 1;
-						} else if(minV == count[tab.get(j)]) {
-							minC++;
-						}
+						} 
 					}
-					
-//					if(minC > 1 && minV > 0) {
-						minV = -1;
-						for(int j = 0;j<tab.size();j++) {
-							int last = list[tab.get(j)] == null || list[tab.get(j)].isEmpty()? Integer.MAX_VALUE:list[tab.get(j)].peek();
-							if(minV < last) {
-								minV = last;
-								minI = j;
-							} 
-						}
-//					}
-//					System.out.println("j="+minI);
 					tab.remove(minI);
 					tab.add(seq[i]);
 					cnt++;
 				}
 			}
-			count[seq[i]]--;
 			list[seq[i]].poll();
-//			System.out.println("end "+tab.toString());
 		}
 		
 		bw.write(String.valueOf(cnt));
