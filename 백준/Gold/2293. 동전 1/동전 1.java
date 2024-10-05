dbb41d9f4b4c61e4eb5b0bb0bbe9fb5a0f39fe99
Yerim <77157624+rime-y@users.noreply.github.com>
2024-10-05 21:54:51 +0900

    [Gold IV] Title: 동전 1, Time: 156 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[k+1][n];
+		int[] ns = new int[n];
+		
+		for(int i = 0;i<n;i++)
+			ns[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(ns);
+		for(int i = 1;i<=k;i++) {
+			for(int j = 0;j<n;j++) {
+				if(i-ns[j] < 0) break;
+				for(int l=0;l<=j;l++) {
+					dp[i][j] += dp[i-ns[j]][l];
+				}
+				if(i == ns[j])
+					dp[i][j] += 1;
+			}
+		}
+		
+//		for(int i = 0;i<=k;i++) {
+//			for(int j = 0;j<n;j++) {
+//				System.out.print(dp[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		int sum = 0;
+		for(int j=0;j<n;j++)
+			sum += dp[k][j];
+		
+		System.out.println(sum);
+		
+	}
+}
